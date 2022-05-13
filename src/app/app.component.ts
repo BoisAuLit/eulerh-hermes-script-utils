c02084042f084ea8b29a7a9aa5f304a3ad9f02e1
BLI <bohao.li.ext@eulerhermes.com>
2022-05-13 15:01:36 +0200

    POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as _ from "lodash";
+import { Parser } from "json2csv";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'euler-hermes-script-utils';
+  inputStr = "";
+  outputStr: string = "";
+
+  transform() {
+    const inputJson = JSON.parse(this.inputStr);
+    const headersArray = inputJson[0];
+    const permissions = inputJson[1].map((dataArray: any[]) =>
+      _.zipObject(headersArray, dataArray)
+    );
+    const json2csvParser = new Parser({ quote: "" });
+    const csvStr = json2csvParser.parse(permissions);
+    this.outputStr = csvStr
+  }
 }
